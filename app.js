bc100bd953be5fc30ede9bf86ffd3fb67fc7a177
Nayeem29 <nayeembracu29@gmail.com>
2022-03-01 17:13:57 +0600

    sensor-loop

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const search = document.querySelector('.search-btn');
 let cartDiv = document.querySelector('#phone-cart');
 let detailsDiv = document.querySelector('#phone-details');
 let showLess = true;
+const spinner = document.querySelector('#spin');
 
 let showMoreBtn = document.querySelector('#show');
 showMoreBtn.style.display = 'none';
+spinner.style.display = 'none';
 
+// on load and back button function 
 const initial = () => {
     detailsDiv.textContent = '';
     const url = 'https://openapi.programming-hero.com/api/phones?';
@@ -15,8 +18,15 @@ const initial = () => {
         .then(data => displayPhone(data))
 }
 
+// toggole function 
+const toggleSpin = displaySpinner => {
+    spinner.style.display = displaySpinner;
+}
+
+//search btn function 
 search.addEventListener('click', () => {
     const inputValue = input.value;
+    toggleSpin('block');
     if ((inputValue === '')) {
         alert("Please provide your prefered gadget Brand");
     } else {
@@ -25,6 +35,7 @@ search.addEventListener('click', () => {
 
 })
 
+// getting data from api for phone brand 
 const onLoad = (brandName) => {
     input.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${brandName}`;
@@ -33,14 +44,22 @@ const onLoad = (brandName) => {
         .then(data => displayPhone(data))
 }
 
+
+// display phone brand and name 
 const displayPhone = (spec) => {
     console.log(spec.data);
-
-    let count = 0;
-    const phoneSummary = spec.data;
+    cartDiv.textContent = '';
+    let phoneSummary = spec.data;
     if (phoneSummary.length === 0) {
         alert('Not Available in our store');
     } else {
+        if ((phoneSummary.length > 20) && showLess) {
+            console.log(phoneSummary.length);
+            phoneSummary = phoneSummary.slice(0, 20);
+
+        }
+
+        console.log(phoneSummary);
         phoneSummary.forEach(brand => {
             const phoneCard = document.createElement('div');
             phoneCard.innerHTML = `     
@@ -56,16 +75,15 @@ const displayPhone = (spec) => {
         `;
 
             cartDiv.appendChild(phoneCard);
-            count++;
-            if (count > 20 && showLess === true) {
-                showMoreBtn.style.display = 'block';
-                showMoreBtn.addEventListener('click', () => {
-                    loadMore();
-                })
 
-            }
         })
     }
+    toggleSpin('none');
+    detailsDiv.textContent = '';
+    showMoreBtn.style.display = 'block';
+    showMoreBtn.addEventListener('click', () => {
+        loadMore(spec.data);
+    })
 
 }
 
@@ -76,34 +94,45 @@ const loadMore = (moreData) => {
 }
 
 
+// search by phone id 
 const phoneId = id => {
     // console.log(id);
+    detailsDiv.textContent = '';
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayDetails(data.data))
 }
 
+
+// display phone details 
 const displayDetails = details => {
-    console.log(details.mainFeatures.storage);
-    console.log(details.mainFeatures.memory);
-    console.log(details.releaseDate);
+    // console.log(details.mainFeatures.storage);
+    // console.log(details.mainFeatures.memory);
+    // console.log(details.releaseDate);
+    const sensors = details.mainFeatures.sensors;
+    console.log(sensors);
     cartDiv.textContent = '';
     const specificationDiv = document.createElement('div');
     specificationDiv.innerHTML = `
     <div class="card text-center mb-3">
       <div class="card-body">
-        <h2 class="card-title my-3">Phone: ${details.name}</h2>
+        <h2 class="card-title my-3">Phone: </h2><p>${details.name}</p>
         <img src="${details.image}" class="card-img-top w-25" alt="...">
-        <h4 class="card-text my-3">Release Date:${details.releaseDate}</h4>
-        <h4 class="card-text my-3">Memory:${details.mainFeatures.memory}</h4>
-        <h4 class="card-text my-3">Sensor:${details.mainFeatures.sensors[0]}</h4>
+        <h4 class="card-text my-3">Release Date: </h4><p>${details.releaseDate ? details.releaseDate:'Not available'}</p>
+        <h4 class="card-text my-3">Memory: </h4><p>${details.mainFeatures.memory}</p>
+        <h4 class="card-text my-3">Sensor: </h4><p>${(function loopSensor(sensors){
+            let sensor= '';
+            for(let i=0;i<sensors.length;i++){   
+                 sensor =sensor+ sensors[i]+' , ';
+            }
+            return sensor;
+        })(sensors)}</p>
       </div>
       <button onclick="initial()" type="button" class="btn btn-primary mx-auto w-25" >Back</button>
     </div>
     `;
     detailsDiv.appendChild(specificationDiv);
+    showMoreBtn.style.display = 'none';
 
-
-    // console.log(details.image);
 }
